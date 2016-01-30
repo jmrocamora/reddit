3382b34cbdfa338a6905866220acfd384a05d6be
Jose María Rocamora Pamies <jmrocamora@gmail.com>
2016-01-30 18:49:11 +0100

    Añadido y configurado acts_as_votable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :links
+  resources :links do
+    member do
+      put "like", to:    "links#upvote"
+      put "dislike", to: "links#downvote"
+     end
+    end
 
   # Definimos la root_url para Device
   root to: "links#index"
